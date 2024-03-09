36450aaa7c3e3147f6767520e74cd6d23baf4623
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-03-09 04:47:19 +0000

    did umbrella

--- app.rb
+++ app.rb
@@ -15,7 +15,8 @@ end
 post("/umbrella_result") do
   @local = params.fetch("user_local")
   gmaps = ENV.fetch("GMAPS_KEY")
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps}"
+  @local_url = @local.gsub(" ","+")
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@local_url}&key=#{gmaps}"
   
   raw_gmaps = HTTP.get(gmaps_url)
   parsed_gmaps = JSON.parse(raw_gmaps)
@@ -33,35 +34,40 @@ post("/umbrella_result") do
   parsed_pw = JSON.parse(raw_pw)
   @current_hash = parsed_pw.fetch("currently")
   @the_temp = @current_hash.fetch("temperature")
-
-  @minute_hash = parsed_pw.fetch("minuteley", false)
+  @minute_hash = parsed_pw.fetch("hourly")
   @weather_summary = @minute_hash.fetch("summary")
 
-  @hour_hash = parsed_pw.fetch("hourly")
-  hour_array = hour_hash.fetch("data")
+  #@hour_hash = parsed_pw.fetch("hourly")
+  hour_array = @minute_hash.fetch("data")
+  @hour_hash = hour_array.at(0)
   next_twelve = hour_array[1..12]
-  precip_threshold = 0.10
+
   
   
-  any_precip = false
+  @any_precip = false
+  
+
 
   next_twelve.each do |the_hour|
     precip_probability = the_hour.fetch("precipProbability")
+    
 
-    if precip_probability > precip_threshold
-      any_precip = true
+    if precip_probability > 0.10
+      @any_precip = true
       precip_time = Time.at(the_hour.fetch("time"))
       seconds_later = precip_time - Time.now
       hours_later = seconds_later / 60 / 60
     end
 
-    if any_precip
+    if @any_precip == true
       @result = "You might want to carry an umbrella!"
     else
       @result = "You probaly won't need an umbrella today."
     end
+  end
 
     cookies["last_location"] = @local
     cookies["last_lat"] = @lat
     cookies["last_long"] = @lng
   erb(:umbrella_result)
+end
